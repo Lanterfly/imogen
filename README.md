d27ab85da64908191eb3a0860dac322f27c935a1
Ethan Raymond <dev@ethanraymond.com>
2023-11-10 10:51:04 -0700

    Added validation command.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Imogen
+
+## Configuration
+
+| Field           | Description | Is Required?   |
+|-----------------|-------------|----------------|
+| jobs            |             | Yes            |
+| jobs[#].time    |             | Yes            |
+| jobs[#].command |             | Yes            |
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
 import { program } from 'commander';
 
-program.name('imogen').version("0.1.0");
+import {validateConfig} from "./src/validateConfig.js";
+
+program
+    .name('imogen')
+    .version("0.1.0");
+program.command('validate')
+	.description('Validate a configuration file.')
+	.option('-c, --config <string>', 'Config file', 'test.config.json')
+	.action(
+		(str, options) => {
+			// Load Options
+			const opts = options.opts();
+
+			// Load config
+			let config = fs.readFileSync(opts.config);
+			config = JSON.parse(config);
+
+			// Validate Config
+			validateConfig(config);
+			console.log("The config file is valid.");
+		},
+	);
 
 program.parse();
