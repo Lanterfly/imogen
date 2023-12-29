8b012de7d2e5b03d3ffcf9db905ea117510fabda
Ethan Raymond <dev@ethanraymond.com>
2023-12-29 13:14:39 -0700

    Extracted scheduling code.

--- src/JobRun.js
+++ src/JobRun.js
@@ -6,6 +6,76 @@ import {exec} from "child_process";
 
 import {loadConfig, validateConfig} from "./ConfigUtil.js";
 
+const scheduleJob = (logger, job) => {
+	let isRunning = false;
+
+	logger.info(`Scheduling job '${job.name}'. Running on schedule: '${job.time}'`);
+	schedule.scheduleJob(
+		job.time,
+		async () => {
+			const startTime = DateTime.now();
+
+			if (!(isRunning && job.simultaneous)) {
+				if (job.name !== undefined) {
+					logger.info(`Starting execution of job "${job.name}".`);
+				} else {
+					logger.info(`Starting execution of job "${job.command}".`);
+				}
+				isRunning = true;
+				exec(
+					job.command,
+					(err, stdout, stderr) => {
+						// Log STDOUT and STDERR
+						if (stdout) {
+							logger.info(`Execution STDOUT:\n${stdout}`);
+						}
+						if (stderr) {
+							logger.info(`Execution STDERR:\n${stderr}`);
+						}
+
+						if (err) {
+							logger.error(`Failed to run job: ${err}`);
+						} else {
+							// Log job completion
+							if (job.name !== undefined) {
+								logger.info(`Finished execution of job "${job.name}".`);
+							} else {
+								logger.info(`Finished execution of job "${job.command}".`);
+							}
+
+							// Output STDOUT & STDERR Files
+							const filePrefix = `${startTime.toFormat('yyyyMMdd_HHmm')}_${job.name}`;
+							if (config.record.writeStdOut) {
+								fs.writeFile(
+									`${config.record.directory}/${filePrefix}_stdout.log`,
+									stdout || '',
+									(error) => {
+										if (error) {
+											logger.error(`Failed to write STDOUT file for job '${job.name}': ${error}`);
+										}
+									},
+								);
+							}
+							if (config.record.writeStdErr) {
+								fs.writeFile(
+									`${config.record.directory}/${filePrefix}_stderr.log`,
+									stderr || '',
+									(error) => {
+										if (error) {
+											logger.error(`Failed to write STDERR file for job '${job.name}': ${error}`);
+										}
+									},
+								);
+							}
+						}
+					},
+				);
+				isRunning = false;
+			}
+		},
+	);
+};
+
 export default (str, options) => {
 	// Load Config
 	const config = loadConfig(options.opts());
@@ -18,74 +88,6 @@ export default (str, options) => {
 
 	// Schedule Jobs
 	config.jobs.forEach(
-		(job) => {
-			let isRunning = false;
-
-			logger.info(`Scheduling job '${job.name}'. Running on schedule: '${job.time}'`);
-			schedule.scheduleJob(
-				job.time,
-				async () => {
-					const startTime = DateTime.now();
-
-					if (!(isRunning && job.simultaneous)) {
-						if (job.name !== undefined) {
-							logger.info(`Starting execution of job "${job.name}".`);
-						} else {
-							logger.info(`Starting execution of job "${job.command}".`);
-						}
-						isRunning = true;
-						exec(
-							job.command,
-							(err, stdout, stderr) => {
-								// Log STDOUT and STDERR
-								if (stdout) {
-									logger.info(`Execution STDOUT:\n${stdout}`);
-								}
-								if (stderr) {
-									logger.info(`Execution STDERR:\n${stderr}`);
-								}
-
-								if (err) {
-									logger.error(`Failed to run job: ${err}`);
-								} else {
-									// Log job completion
-									if (job.name !== undefined) {
-										logger.info(`Finished execution of job "${job.name}".`);
-									} else {
-										logger.info(`Finished execution of job "${job.command}".`);
-									}
-
-									// Output STDOUT & STDERR Files
-									const filePrefix = `${startTime.toFormat('yyyyMMdd_HHmm')}_${job.name}`;
-									if (config.record.writeStdOut) {
-										fs.writeFile(
-											`${config.record.directory}/${filePrefix}_stdout.log`,
-											stdout || '',
-											(error) => {
-												if (error) {
-													logger.error(`Failed to write STDOUT file for job '${job.name}': ${error}`);
-												}
-											},
-										);
-									}
-									if (config.record.writeStdErr) {
-										fs.writeFile(
-											`${config.record.directory}/${filePrefix}_stderr.log`,
-											stderr || '',
-											(error) => {
-												if (error) {
-													logger.error(`Failed to write STDERR file for job '${job.name}': ${error}`);
-												}
-											},
-										);
-									}
-								}
-							},
-						);
-						isRunning = false;
-					}
-				},
-			);
-		},
+		(job) => scheduleJob(logger, job),
 	);
 };
