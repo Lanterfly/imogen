5d51fffd0ad574694290ea71303c9caa795176c6
Ethan Raymond <dev@ethanraymond.com>
2023-12-29 13:52:21 -0700

    Added unit tests for onCompletedJob.

---
+++ src/JobRun.test.js
@@ -0,0 +1,92 @@
+import { onCompletedJob } from "./JobRun.js";
+import {DateTime} from "luxon";
+
+const MOCK_LOGGER = {
+	info: () => {},
+	error: () => {},
+};
+
+describe(
+	'onCompletedJob()',
+	() => {
+		test(
+			'With STDOUT',
+			() => expect(() => onCompletedJob(
+				undefined,
+				'stdout',
+				undefined,
+				{
+					"record": {}
+				},
+				MOCK_LOGGER,
+				{ name: 'job' },
+				DateTime.now(),
+			)).not.toThrow()
+		);
+		test(
+			'With STDERR',
+			() => expect(() => onCompletedJob(
+				undefined,
+				undefined,
+				'stderr',
+				{
+					"record": {}
+				},
+				MOCK_LOGGER,
+				{ name: 'job' },
+				DateTime.now(),
+			)).not.toThrow()
+		);
+		test(
+			'Failed Run',
+			() => expect(() => onCompletedJob(
+				'Error',
+				undefined,
+				undefined,
+				{
+					"record": {}
+				},
+				MOCK_LOGGER,
+				{ name: 'job' },
+				DateTime.now(),
+			)).not.toThrow()
+		);
+		describe(
+			'Successful Run',
+			() => {
+				test(
+					'Write STDOUT',
+					() => expect(() => onCompletedJob(
+						undefined,
+						'stdout',
+						undefined,
+						{
+							"record": {
+								'writeStdOut': true,
+							}
+						},
+						MOCK_LOGGER,
+						{ name: 'job' },
+						DateTime.now(),
+					)).not.toThrow()
+				);
+				test(
+					'Write STDERR',
+					() => expect(() => onCompletedJob(
+						undefined,
+						undefined,
+						'stderr',
+						{
+							"record": {
+								'writeStdErr': true,
+							}
+						},
+						MOCK_LOGGER,
+						{ name: 'job' },
+						DateTime.now(),
+					)).not.toThrow()
+				);
+			},
+		);
+	},
+);
