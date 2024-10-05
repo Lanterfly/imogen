7bdbe6072d3007baafba49942172fab32c3c7a34
Ethan <dev@ethanraymond.com>
2024-10-05 12:19:19 -0600

    Reorganized.

--- src/index.js
+++ src/index.js
@@ -12,83 +12,77 @@ export const loadConfig = (options) => {
 	// Load Options
 	const opts = options.opts();
 
-	// Set Default Config
-	const DEFAULT_CONFIG = {};
-
 	// Load config
 	const config = fs.readFileSync(opts.config);
-	return {
-		...DEFAULT_CONFIG,
-		...JSON.parse(config),
-	};
+	return JSON.parse(config);
+};
+
+const run = (str, options) => {
+	const logger = pino();
+
+	logger.info("Starting Imogen...");
+
+	logger.info('Reading config file...');
+	const config = loadConfig(options);
+	logger.info('Read config file.');
+
+	logger.info('Registering jobs...');
+	if (_.isArray(config.jobs) && config.jobs.length > 0) {
+		const errors = [];
+		for (let i = 0; i < config.jobs.length; i++) {
+			const job = config.jobs[i];
+
+			if (!(_.isString(job.name) && job.name.length > 0 && job.name.match(/([a-zA-Z0-9-_])+/))) {
+				errors.push(`Job #${i} has an invalid name.`);
+			}
+			if (!(_.isString(job.command) && job.command.length > 0)) {
+				errors.push(`Job #${i} has an invalid command.`);
+			}
+
+		}
+		if (errors.length === 0) {
+			for (const job of config.jobs) {
+				schedule.scheduleJob(
+					job.time,
+					() => {
+						logger.info(`Running job ${job.name}`);
+						exec(
+							job.command,
+							(err, stdout, stderr) => {
+								// Log STDOUT and STDERR
+								if (stdout) {
+									logger.info(`Job '${job.name}' STDOUT:\n${stdout}`);
+								}
+								if (stderr) {
+									logger.info(`Job '${job.name}' STDERR:\n${stderr}`);
+								}
+
+								if (err) {
+									logger.error(`Failed to run job: ${err}`);
+								} else {
+									// Log job completion
+									logger.info(`Finished execution of job "${job.name}".`);
+								}
+							},
+						);
+					}
+				)
+			}
+		} else {
+			for (const error of errors) {
+				logger.error(error);
+			}
+		}
+		logger.info("Registered Jobs");
+	} else {
+		logger.error("No jobs to run.");
+	}
 };
 
 
 program.name('imogen')
 	.version('2.0.0')
 	.option('-c, --config <string>', 'Config file', 'imogen.config.json')
-	.action(
-		(str, options) => {
-			const logger = pino();
-
-			logger.info("Starting Imogen...");
-
-			logger.info('Reading config file...');
-			const config = loadConfig(options);
-			logger.info('Read config file.');
-
-			logger.info('Registering jobs...');
-			if (_.isArray(config.jobs) && config.jobs.length > 0) {
-				const errors = [];
-				for (let i = 0; i < config.jobs.length; i++) {
-					const job = config.jobs[i];
-
-					if (!(_.isString(job.name) && job.name.length > 0 && job.name.match(/([a-zA-Z0-9-_])+/))) {
-						errors.push(`Job #${i} has an invalid name.`);
-					}
-					if (!(_.isString(job.command) && job.command.length > 0)) {
-						errors.push(`Job #${i} has an invalid command.`);
-					}
-
-				}
-				if (errors.length === 0) {
-					for (const job of config.jobs) {
-						schedule.scheduleJob(
-							job.time,
-							() => {
-								logger.info(`Running job ${job.name}`);
-								exec(
-									job.command,
-									(err, stdout, stderr) => {
-										// Log STDOUT and STDERR
-										if (stdout) {
-											logger.info(`Job '${job.name}' STDOUT:\n${stdout}`);
-										}
-										if (stderr) {
-											logger.info(`Job '${job.name}' STDERR:\n${stderr}`);
-										}
-
-										if (err) {
-											logger.error(`Failed to run job: ${err}`);
-										} else {
-											// Log job completion
-											logger.info(`Finished execution of job "${job.name}".`);
-										}
-									},
-								);
-							}
-						)
-					}
-				} else {
-					for (const error of errors) {
-						logger.error(error);
-					}
-				}
-				logger.info("Registered Jobs");
-			} else {
-				logger.error("No jobs to run.");
-			}
-		},
-	);
+	.action(run);
 
 program.parse();
