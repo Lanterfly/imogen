cef180f814166df29ead48bee77d7054f05ae4ee
Ethan <dev@ethanraymond.com>
2024-10-20 15:42:33 -0700

    Lint fixes.

--- src/jobs.js
+++ src/jobs.js
@@ -3,75 +3,75 @@ import schedule from "node-schedule";
 import {exec} from "child_process";
 
 export const scheduleJobs = (logger, config, db) => {
-    logger.info('Registering jobs...');
-    if (_.isArray(config.jobs) && config.jobs.length > 0) {
-        const errors = [];
-        for (let i = 0; i < config.jobs.length; i++) {
-            const job = config.jobs[i];
+	logger.info('Registering jobs...');
+	if (_.isArray(config.jobs) && config.jobs.length > 0) {
+		const errors = [];
+		for (let i = 0; i < config.jobs.length; i++) {
+			const job = config.jobs[i];
 
-            if (!(_.isString(job.name) && job.name.length > 0 && job.name.match(/([a-zA-Z0-9-_])+/))) {
-                errors.push(`Job #${i} has an invalid name.`);
-            }
-            if (!(_.isString(job.time) && job.time.length > 0)) {
-                errors.push(`Job #${i} has an invalid time.`);
-            }
-            if (!(_.isString(job.command) && job.command.length > 0)) {
-                errors.push(`Job #${i} has an invalid command.`);
-            }
+			if (!(_.isString(job.name) && job.name.length > 0 && job.name.match(/([a-zA-Z0-9-_])+/))) {
+				errors.push(`Job #${i} has an invalid name.`);
+			}
+			if (!(_.isString(job.time) && job.time.length > 0)) {
+				errors.push(`Job #${i} has an invalid time.`);
+			}
+			if (!(_.isString(job.command) && job.command.length > 0)) {
+				errors.push(`Job #${i} has an invalid command.`);
+			}
 
-        }
-        if (errors.length === 0) {
-            for (const job of config.jobs) {
-                const jobRecord = db.prepare('SELECT * FROM job WHERE name = ?').get(job.name);
-                let doSchedule;
-                if (jobRecord) {
-                    doSchedule = jobRecord.enabled === undefined || jobRecord.enabled === 'true';
-                } else {
-                    db.prepare('INSERT INTO job VALUES (?, ?, ?)').run(job.name, 'true', 'false');
-                    doSchedule = true;
-                }
+		}
+		if (errors.length === 0) {
+			for (const job of config.jobs) {
+				const jobRecord = db.prepare('SELECT * FROM job WHERE name = ?').get(job.name);
+				let doSchedule;
+				if (jobRecord) {
+					doSchedule = jobRecord.enabled === undefined || jobRecord.enabled === 'true';
+				} else {
+					db.prepare('INSERT INTO job VALUES (?, ?, ?)').run(job.name, 'true', 'false');
+					doSchedule = true;
+				}
 
-                if (doSchedule) {
-                    schedule.scheduleJob(
-                        job.time,
-                        () => {
-                            logger.info(`Running job ${job.name}`);
-                            db.prepare('UPDATE job SET running = ? WHERE name = ?').run('true', job.name);
+				if (doSchedule) {
+					schedule.scheduleJob(
+						job.time,
+						() => {
+							logger.info(`Running job ${job.name}`);
+							db.prepare('UPDATE job SET running = ? WHERE name = ?').run('true', job.name);
 
-                            exec(
-                                job.command,
-                                (err, stdout, stderr) => {
-                                    // Log STDOUT and STDERR
-                                    if (stdout) {
-                                        logger.info(`Job '${job.name}' STDOUT:\n${stdout}`);
-                                    }
-                                    if (stderr) {
-                                        logger.info(`Job '${job.name}' STDERR:\n${stderr}`);
-                                    }
+							exec(
+								job.command,
+								(err, stdout, stderr) => {
+									// Log STDOUT and STDERR
+									if (stdout) {
+										logger.info(`Job '${job.name}' STDOUT:\n${stdout}`);
+									}
+									if (stderr) {
+										logger.info(`Job '${job.name}' STDERR:\n${stderr}`);
+									}
 
-                                    if (err) {
-                                        logger.error(`Failed to run job: ${err}`);
-                                    } else {
-                                        // Log job completion
-                                        db.prepare('UPDATE job SET running = ? WHERE name = ?').run('false', job.name);
-                                        logger.info(`Finished execution of job "${job.name}".`);
-                                    }
-                                },
-                            );
-                        }
-                    );
-                }
-            }
-            logger.info("Registered Jobs");
-            return true;
-        } else {
-            for (const error of errors) {
-                logger.error(error);
-            }
-            return false;
-        }
-    } else {
-        logger.error("No jobs to run.");
-        return false;
-    }
+									if (err) {
+										logger.error(`Failed to run job: ${err}`);
+									} else {
+										// Log job completion
+										db.prepare('UPDATE job SET running = ? WHERE name = ?').run('false', job.name);
+										logger.info(`Finished execution of job "${job.name}".`);
+									}
+								},
+							);
+						}
+					);
+				}
+			}
+			logger.info("Registered Jobs");
+			return true;
+		} else {
+			for (const error of errors) {
+				logger.error(error);
+			}
+			return false;
+		}
+	} else {
+		logger.error("No jobs to run.");
+		return false;
+	}
 }
--- src/server.js
+++ src/server.js
@@ -2,63 +2,63 @@ import http from "http";
 import querystring from "querystring";
 
 export const requestListener = (req, res, db) => {
-    res.setHeader("Content-Type", "application/json");
+	res.setHeader("Content-Type", "application/json");
 
-    const urlParameters = querystring.parse(req.url.substring(req.url.indexOf('?') + 1));
+	const urlParameters = querystring.parse(req.url.substring(req.url.indexOf('?') + 1));
 
-    if (req.url.startsWith('/isEnabled')) {
-        const name = urlParameters.name;
-        if (name === undefined) {
-            res.writeHead(400);
-            res.end(JSON.stringify({ 'error': 'No job name provided.' }));
-            return;
-        }
+	if (req.url.startsWith('/isEnabled')) {
+		const name = urlParameters.name;
+		if (name === undefined) {
+			res.writeHead(400);
+			res.end(JSON.stringify({ 'error': 'No job name provided.' }));
+			return;
+		}
 
-        const jobRecord = db.prepare('SELECT * FROM job WHERE name = ?').get(name);
-        if (!jobRecord) {
-            res.writeHead(400);
-            res.end(JSON.stringify({ 'error': `Job ${name} does not exist.` }));
-            return;
-        }
-        const enabled = jobRecord.enabled === 'true';
+		const jobRecord = db.prepare('SELECT * FROM job WHERE name = ?').get(name);
+		if (!jobRecord) {
+			res.writeHead(400);
+			res.end(JSON.stringify({ 'error': `Job ${name} does not exist.` }));
+			return;
+		}
+		const enabled = jobRecord.enabled === 'true';
 
-        res.writeHead(200);
-        res.end(JSON.stringify({ enabled }));
-    } else if (req.url.startsWith('/isRunning')) {
-        const name = urlParameters.name;
-        if (name === undefined) {
-            res.writeHead(400);
-            res.end(JSON.stringify({ 'error': 'No job name provided.' }));
-            return;
-        }
+		res.writeHead(200);
+		res.end(JSON.stringify({ enabled }));
+	} else if (req.url.startsWith('/isRunning')) {
+		const name = urlParameters.name;
+		if (name === undefined) {
+			res.writeHead(400);
+			res.end(JSON.stringify({ 'error': 'No job name provided.' }));
+			return;
+		}
 
-        const jobRecord = db.prepare('SELECT * FROM job WHERE name = ?').get(name);
-        if (!jobRecord) {
-            res.writeHead(400);
-            res.end(JSON.stringify({ 'error': `Job ${name} does not exist.` }));
-            return;
-        }
-        const running = jobRecord.running === 'true';
+		const jobRecord = db.prepare('SELECT * FROM job WHERE name = ?').get(name);
+		if (!jobRecord) {
+			res.writeHead(400);
+			res.end(JSON.stringify({ 'error': `Job ${name} does not exist.` }));
+			return;
+		}
+		const running = jobRecord.running === 'true';
 
-        res.writeHead(200);
-        res.end(JSON.stringify({ running }));
-    } else {
-        res.writeHead(404);
-        res.end(JSON.stringify({ 'error': 'Unknown route.' }));
-    }
+		res.writeHead(200);
+		res.end(JSON.stringify({ running }));
+	} else {
+		res.writeHead(404);
+		res.end(JSON.stringify({ 'error': 'Unknown route.' }));
+	}
 };
 
 export const startServer = (logger, config, db) => {
-    logger.info("Starting server...");
+	logger.info("Starting server...");
 
-    const bindHostname = config.server.bindHostname || 'localhost';
-    const bindPort = parseInt(config.server.bindPort) || 16061;
+	const bindHostname = config.server.bindHostname || 'localhost';
+	const bindPort = parseInt(config.server.bindPort) || 16061;
 
-    // Start Server
-    const server = http.createServer(
-        (req, res) => requestListener(req, res, db)
-    );
-    server.listen(bindPort, bindHostname, () => {
-        logger.info(`Started server on http://${bindHostname}:${bindPort}`);
-    });
+	// Start Server
+	const server = http.createServer(
+		(req, res) => requestListener(req, res, db)
+	);
+	server.listen(bindPort, bindHostname, () => {
+		logger.info(`Started server on http://${bindHostname}:${bindPort}`);
+	});
 };
