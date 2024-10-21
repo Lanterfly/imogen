83905571dab23223f432193f986255e8a202475a
Ethan <dev@ethanraymond.com>
2024-10-20 17:14:09 -0700

    Added simple home page.

---
+++ docs/src/components/Header.jsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useState } from 'react'
+import {
+    Dialog,
+    DialogPanel,
+    PopoverGroup,
+} from '@headlessui/react'
+import {
+    Bars3Icon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+
+export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <header className="bg-white">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img
+                            alt=""
+                            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                            className="h-8 w-auto"
+                        />
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+                    </button>
+                </div>
+                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        Get Started
+                    </a>
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        Documentation
+                    </a>
+                </PopoverGroup>
+            </nav>
+        </header>
+    )
+}
--- docs/src/pages/_app.js
+++ docs/src/pages/_app.js
@@ -1,5 +1,12 @@
 import "#/styles/globals.css";
 
+import Header from "#/components/Header.jsx";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />
+        </>
+    );
 }
