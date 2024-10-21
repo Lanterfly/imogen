a83c68b630c6ef1932b5f4fe245ad87a0e3ca302
Ethan <dev@ethanraymond.com>
2024-10-20 18:21:29 -0700

    Added intro page.

--- docs/src/pages/_app.js
+++ docs/src/pages/_app.js
@@ -2,6 +2,8 @@ import 'bootswatch/dist/flatly/bootstrap.min.css';
 
 import Header from "#/components/Header.jsx";
 
+import './_app.css';
+
 export default function App({ Component, pageProps }) {
     return (
         <>
